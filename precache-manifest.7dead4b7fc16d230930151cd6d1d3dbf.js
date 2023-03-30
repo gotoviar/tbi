4d217b39b14f2fb0a279eb9fe17578a4df9d9c70
Victor Stanchevici <vstanchevici@yahoo.com>
2023-03-30 16:48:06 +0300

    Add tools objects

---
+++ precache-manifest.7dead4b7fc16d230930151cd6d1d3dbf.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3b1260db2bc9ee7dc70298df9ca33fa2",
+    "url": "/index.html"
+  },
+  {
+    "revision": "84f816ee9182532093d4",
+    "url": "/static/css/2.261d82c3.chunk.css"
+  },
+  {
+    "revision": "d5f64a4161536cfa4d78",
+    "url": "/static/css/main.904ee16c.chunk.css"
+  },
+  {
+    "revision": "84f816ee9182532093d4",
+    "url": "/static/js/2.173d808a.chunk.js"
+  },
+  {
+    "revision": "0b31fb14eee500646560caa49543f84c",
+    "url": "/static/js/2.173d808a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5c93ba042a223052583b",
+    "url": "/static/js/3.a55fdabc.chunk.js"
+  },
+  {
+    "revision": "d5f64a4161536cfa4d78",
+    "url": "/static/js/main.7c863af9.chunk.js"
+  },
+  {
+    "revision": "4e0e34f265fae8f33b01b27ae29d9d6f",
+    "url": "/static/js/main.7c863af9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "271a1ed0787a7d69eedc",
+    "url": "/static/js/runtime~main.7ec23b4b.js"
+  },
+  {
+    "revision": "b7c9e1e479de3b53f1e4e30ebac2403a",
+    "url": "/static/media/slick.b7c9e1e4.woff"
+  },
+  {
+    "revision": "ced611daf7709cc778da928fec876475",
+    "url": "/static/media/slick.ced611da.eot"
+  },
+  {
+    "revision": "d41f55a78e6f49a5512878df1737e58a",
+    "url": "/static/media/slick.d41f55a7.ttf"
+  },
+  {
+    "revision": "f97e3bbf73254b0112091d0192f17aec",
+    "url": "/static/media/slick.f97e3bbf.svg"
+  }
+]);
